560098fcc2e7c66f3472e9d4e9bd3501ac2216ec
Paola Novick <paola.novick@davinci.edu.ar>
2025-11-10 15:48:25 -0300

    login

--- server.js
+++ server.js
@@ -13,7 +13,8 @@ dotenv.config();
 conectarDB();
 
 const app = express();
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
 // ✅ CORS
 app.use(
